393182d7907f90b22c2ea9db71c74f1829a70b13
AlexeyK <alexey.kononov@checkmarx.com>
2020-09-22 16:24:00 +0300

    Warn instead of throwing an exception fix.

--- src/main/java/com/cx/restclient/configuration/PropertyFileLoader.java
+++ src/main/java/com/cx/restclient/configuration/PropertyFileLoader.java
@@ -20,9 +20,10 @@ public class PropertyFileLoader {
 
     /**
      * Loads properties from resources.
+     *
      * @param filenames list of resource filenames to load properties from.
      *                  If the same property appears several times in the files, the property value from a file will be overridden with the value from the next file.
-     * @throws  CxClientException if no filenames is provided, or if an error occurred while loading a file.
+     * @throws CxClientException if no filenames is provided, or if an error occurred while loading a file.
      */
     public PropertyFileLoader(String... filenames) {
         if (filenames.length == 0) {
@@ -39,11 +40,13 @@ public class PropertyFileLoader {
 
     private Properties getPropertiesFromResource(URL resourceUrl) {
         Properties result = new Properties();
-        try (FileReader fileReader = new FileReader(resourceUrl.getPath())) {
-            result.load(fileReader);
-            log.debug("Loaded properties from {}", resourceUrl);
-        } catch (IOException e) {
-            log.warn("Unable to load properties from {}, skipping. {}", resourceUrl, e.getMessage());
+        if (resourceUrl != null) {
+            log.debug("Loading properties from resource: {}", resourceUrl);
+            try (FileReader fileReader = new FileReader(resourceUrl.getPath())) {
+                result.load(fileReader);
+            } catch (IOException e) {
+                throw new CxClientException(String.format("Error loading the '%s' resource.", resourceUrl), e);
+            }
         }
         return result;
     }
@@ -52,10 +55,11 @@ public class PropertyFileLoader {
         log.debug("Getting resource URL for a property file: {}", filename);
         ClassLoader classLoader = this.getClass().getClassLoader();
         URL result = classLoader.getResource(filename);
-        if (result == null) {
-            throw new CxClientException(String.format("Resource '%s' is not found.", filename));
+        if (result != null) {
+            log.debug("Property file URL: {}", result);
+        } else {
+            log.warn("Unable to find resource: {}, skipping.", filename);
         }
-        log.debug("Property file URL: {}", result);
         return result;
     }
 
