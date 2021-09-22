b68d845f0a2b0d4d5ae72ed39df7326ee338351e
SubhadraSahoo <75673851+SubhadraSahoo@users.noreply.github.com>
2021-09-22 17:02:23 +0530

    Completed the feature Continue build for Jenkins plugin. AB#2829 (#166)
    Handled the exceptions in such a way that if scan time out occurs and continue build option is selected , and scan time out exception happens, succeeds the build. and last scan build report is shown.
    For a new project , as previous reports are not available handled the scenario gracefully by logging the message for user

--- src/main/java/com/cx/restclient/configuration/CxScanConfig.java
+++ src/main/java/com/cx/restclient/configuration/CxScanConfig.java
@@ -56,6 +56,7 @@ public class CxScanConfig implements Serializable {
     private String teamId;
     private Boolean denyProject = false;
     private Boolean hideResults = false;
+    private Boolean continueBuild = false;
     private Boolean isPublic = true;
     private Boolean forceScan = false;
     private String presetName;
@@ -620,6 +621,14 @@ public class CxScanConfig implements Serializable {
         this.hideResults = hideResults;
     }
 
+    public Boolean getContinueBuild() {
+        return continueBuild;
+    }
+
+    public void setContinueBuild(Boolean continueBuild) {
+        this.continueBuild = continueBuild;
+    }
+    
 
     public Boolean isAvoidDuplicateProjectScans() {
         return avoidDuplicateProjectScans;
