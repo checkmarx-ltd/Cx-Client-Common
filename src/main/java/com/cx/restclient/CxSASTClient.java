69fe8e0d31116fdc68348daec999f171aacc9164
umeshwaghode <umesh_waghode@persistent.com>
2022-03-08 16:31:09 +0530

    Avoid dup scan will not result into error

--- src/main/java/com/cx/restclient/CxSASTClient.java
+++ src/main/java/com/cx/restclient/CxSASTClient.java
@@ -268,9 +268,11 @@ public class CxSASTClient extends LegacyClient implements Scanner {
 
     //CREATE SAST scan
     private void createSASTScan(long projectId) {
+    	boolean dupScanFound = false;
         try {
             log.info("-----------------------------------Create CxSAST Scan:------------------------------------");
             if (config.isAvoidDuplicateProjectScans() != null && config.isAvoidDuplicateProjectScans() && projectHasQueuedScans(projectId)) {
+            	dupScanFound = true;
                 throw new CxClientException("\nAvoid duplicate project scans in queue\n");
             }
             if (config.getRemoteType() == null) { //scan is local
@@ -285,7 +287,7 @@ public class CxSASTClient extends LegacyClient implements Scanner {
         } catch (Exception e) {
             log.error(e.getMessage());
             setState(State.FAILED);
-            if(!config.getContinueBuild()) {
+            if(!config.getContinueBuild() || (!dupScanFound)) {
             sastResults.setException(new CxClientException(e));
             }
         }
