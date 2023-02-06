3d3ac330ecfaa236019687dabe8584a905a36fa9
Subhadra.Sahoo@checkmarx.com <subhadra_sahoo@persistent.com>
2023-02-06 16:44:23 +0530

    Checking in the missed class as newly was added

---
+++ src/main/java/com/cx/restclient/dto/DummyResults.java
@@ -0,0 +1,13 @@
+package com.cx.restclient.dto;
+
+import java.io.Serializable;
+
+/**
+ * This object is being put in the template data has to be used in report.ftl in scenarios when encodeXSS is called in a scenario, 
+   when the type of result is unknown
+ * @author admin
+ *
+ */
+public class DummyResults extends Results implements Serializable{
+	
+}
