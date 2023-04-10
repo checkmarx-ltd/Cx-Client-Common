c2c6de29f0e09926f32ea0419b404fbfef181ca2
swatipersistent <99341045+swatipersistent@users.noreply.github.com>
2023-04-10 02:36:20 -0700

    Report with results of both SCA and SAST from Jenkins Plugin

--- src/main/java/com/cx/restclient/configuration/CxScanConfig.java
+++ src/main/java/com/cx/restclient/configuration/CxScanConfig.java
@@ -946,6 +946,10 @@ public class CxScanConfig implements Serializable {
 		this.masterBranchProjName = masterBranchProjName;
 	}
 	
+	public boolean getGenerateScaReport() {
+		return generateScaReport;
+	}
+	
 	public boolean isGenerateScaReport() {
 		return generateScaReport;
 	}
@@ -961,6 +965,6 @@ public class CxScanConfig implements Serializable {
 	public void setScaReportFormat(String scaReportFormat) {
 		this.scaReportFormat = scaReportFormat;
 	}
-
+	
 	
 }
